a8b58e6b907f49021177210661c94dd98cff6daa
Michele <micheledatii@gmail.com>
2025-11-30 19:39:22 +0800

    init 2: basic folder structure set up

---
+++ frontend/lib/core/theme/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const red = Color(0xFFE21B3C);
+  static const yellow = Color(0xFFFFA602);
+  static const green = Color(0xFF26890C);
+  static const blue = Color(0xFF1368CE);
+  static const purple = Color(0xFF46178F);
+  static const white = Color(0xFFFFFFFF);
+}
